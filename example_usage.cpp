e8636b5d7cba5745d0d665f49d0d747871c54bfe
unknown <deepraval2905@gmail.com>
2020-08-21 11:52:33 +0530

    Cleaned the code and added better error handling

--- example_usage.cpp
+++ example_usage.cpp
@@ -17,11 +17,11 @@ static void test() {
     // Printing summary of model
     myNN.summary();
     // Training Model
-    myNN.fit_from_csv("iris.csv", true, 100, 0.3, false, 32, true);
+    myNN.fit_from_csv("iris.csv", true, 100, 0.3, false, 2, 32, true);
     // Testing predictions of model
-    assert(machine_learning::argmax(myNN.single_predict({{5,3.4,1.6,0.4}})) == 0);
-    assert(machine_learning::argmax(myNN.single_predict({{6.4,2.9,4.3,1.3}})) == 1);
-    assert(machine_learning::argmax(myNN.single_predict({{6.2,3.4,5.4,2.3}})) == 2);
+    assert(machine_learning::argmax(myNN.single_predict({{5, 3.4, 1.6, 0.4}})) == 0);
+    assert(machine_learning::argmax(myNN.single_predict({{6.4, 2.9, 4.3, 1.3}})) == 1);
+    assert(machine_learning::argmax(myNN.single_predict({{6.2, 3.4, 5.4, 2.3}})) == 2);
     return;
 }
 
