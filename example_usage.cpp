c12204dee6669b191153b3e76e65b828e2f113c0
Deep Raval <deepraval2905@gmail.com>
2020-10-10 20:23:44 +0530

    Code cleanup and fixed typos

--- example_usage.cpp
+++ example_usage.cpp
@@ -9,19 +9,24 @@
 static void test() {
     // Creating network with 3 layers for "iris.csv"
     machine_learning::neural_network::NeuralNetwork myNN =
-    machine_learning::neural_network::NeuralNetwork({
-        {4, "none"}, // First layer with 3 neurons and "none" as activation
-        {6, "relu"}, // Second layer with 6 neurons and "relu" as activation
-        {3, "sigmoid"} // Third layer with 3 neurons and "sigmoid" as activation
-    });
+        machine_learning::neural_network::NeuralNetwork({
+            {4, "none"},  // First layer with 3 neurons and "none" as activation
+            {6,
+             "relu"},  // Second layer with 6 neurons and "relu" as activation
+            {3, "sigmoid"}  // Third layer with 3 neurons and "sigmoid" as
+                            // activation
+        });
     // Printing summary of model
     myNN.summary();
     // Training Model
     myNN.fit_from_csv("iris.csv", true, 100, 0.3, false, 2, 32, true);
     // Testing predictions of model
-    assert(machine_learning::argmax(myNN.single_predict({{5, 3.4, 1.6, 0.4}})) == 0);
-    assert(machine_learning::argmax(myNN.single_predict({{6.4, 2.9, 4.3, 1.3}})) == 1);
-    assert(machine_learning::argmax(myNN.single_predict({{6.2, 3.4, 5.4, 2.3}})) == 2);
+    assert(machine_learning::argmax(
+               myNN.single_predict({{5, 3.4, 1.6, 0.4}})) == 0);
+    assert(machine_learning::argmax(
+               myNN.single_predict({{6.4, 2.9, 4.3, 1.3}})) == 1);
+    assert(machine_learning::argmax(
+               myNN.single_predict({{6.2, 3.4, 5.4, 2.3}})) == 2);
     return;
 }
 
